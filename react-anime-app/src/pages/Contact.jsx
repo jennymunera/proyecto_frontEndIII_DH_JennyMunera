e8ab37293502fe7064253dc9944e149f244273e6
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-11-30 16:28:39 -0500

    Update to Import Component Contact

--- react-anime-app/src/pages/Contact.jsx
+++ react-anime-app/src/pages/Contact.jsx
@@ -1,4 +1,12 @@
+import ContactForm from '../components/ContactForm'; // Ruta correcta
+
 function Contact() {
-    return <h1>Contact Us</h1>;
+    return (
+        <div>
+            <h1>Contact Us</h1>
+            <ContactForm />
+        </div>
+    );
 }
+
 export default Contact;

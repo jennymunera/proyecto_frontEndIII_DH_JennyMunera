595ba3676fbcffbea56c23f4aab5d32f47d710d6
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-11-30 16:50:02 -0500

    add images from animes

--- react-anime-app/src/components/AnimeList.jsx
+++ react-anime-app/src/components/AnimeList.jsx
@@ -17,11 +17,13 @@ function AnimeList() {
     }, []);
 
     return (
-        <div>
+        <div className='anime-list'>
             <h2>Anime List</h2>
             <ul>
                 {animes.map((anime) => (
                     <li key={anime.mal_id}>
+                        <img src={anime.images.jpg.image_url} alt={anime.title} />
+                        <h3>{anime.title}</h3>
                         <Link to = {`/character/${anime.mal_id}`}>{anime.title}</Link>
                         <button onClick={() => addFavorite(anime)}>Add to favorites</button>
                     </li>

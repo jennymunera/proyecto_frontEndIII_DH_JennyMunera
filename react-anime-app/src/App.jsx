adde9e3af95b06fd47dcce2469f95632b89ba1b9
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-11-30 16:49:50 -0500

    add header and footer

--- react-anime-app/src/App.jsx
+++ react-anime-app/src/App.jsx
@@ -5,18 +5,28 @@ import Product from './pages/Product';
 import AnimeDetails from './components/AnimeDetails';
 import Navbar from './components/Navbar';
 import Favorites from './pages/Favorites';
+import './styles/App.css';
 
 function App() {
   return (
     <Router>
+      <header>
+        <h1>Welcome to Anime World</h1>
+        <p>Find your favorite anime and add them to your collection!</p>
+      </header>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/character/:id" element={<AnimeDetails />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/character/:id" element={<AnimeDetails />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+      <footer>
+        <p>&copy; 2024 Anime World. All rights reserved.</p>
+      </footer>
     </Router>
   );
 }

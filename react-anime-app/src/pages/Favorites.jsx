ba4903016e55de9c59e47b8c3fc100ef8fdd1f99
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-11-30 21:39:16 -0500

    modify favorites for card and replace only name list

--- react-anime-app/src/pages/Favorites.jsx
+++ react-anime-app/src/pages/Favorites.jsx
@@ -1,22 +1,33 @@
 import { useFavorites } from '../context/FavoritesContext';
+import '../styles/App.css';
 
 function Favorites() {
     const { favorites, removeFavorite } = useFavorites();
 
     return (
-        <div>
-            <h2>Your Favorites</h2>
+        <div className='favorites'>
+            <h2>Lista de Favoritos</h2>
             {favorites.length === 0 ? (
-                <p>No favorites yet.</p>
+                <p>No se han Agregado Favoritos Aún.</p>
             ) : (
-                <ul>
+                <div className='favorites-grid'>
                     {favorites.map((anime) => (
-                        <li key={anime.mal_id}>
-                            <h3>{anime.title}</h3>
-                            <button onClick={() => removeFavorite(anime.mal_id)}>Remove</button>
-                        </li>
-                ))}
-                </ul>
+                        <div key={anime.mal_id} className="anime-card">
+                            <img
+                                src={anime.images.jpg.image_url}
+                                alt={anime.title}
+                                className="anime-card-image"
+                            />
+                            <div className="anime-card-content">
+                                <h3>{anime.title}</h3>
+                                <button 
+                                    onClick={() => removeFavorite(anime.mal_id)}
+                                    className="remove-button">Eliminar de Favoritos
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             )}
         </div>
     );

7c8d79644d4a0a4b6eb1a7cf9af862ffdd99558a
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-11-30 21:37:31 -0500

    add pagination and modify favorites

--- react-anime-app/src/components/AnimeList.jsx
+++ react-anime-app/src/components/AnimeList.jsx
@@ -1,36 +1,57 @@
-import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useFavorites } from '../context/FavoritesContext';
 
 function AnimeList() {
     const [animes, setAnimes] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1); 
     const { addFavorite } = useFavorites();
 
     useEffect(() => {
         const fetchAnimes = async () => {
-            const response = await fetch('https://api.jikan.moe/v4/anime');
-            const data = await response.json();
-            setAnimes(data.data);
+            try {
+                const response = await fetch(`https://api.jikan.moe/v4/anime?page=${currentPage}`);
+                const data = await response.json();
+                setAnimes(data.data);
+                setTotalPages(data.pagination.last_visible_page);
+            }   catch (error) {
+                console.error('Error Obteniendo lista de Animes: ', error);
+            }
         };
 
         fetchAnimes();
-    }, []);
+    }, [currentPage]);
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    };
+    const handlePreviousPage = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    };
 
     return (
-        <div className='anime-list'>
-            <h2>Anime List</h2>
+        <div className='anime-list'> 
             <ul>
                 {animes.map((anime) => (
                     <li key={anime.mal_id}>
                         <img src={anime.images.jpg.image_url} alt={anime.title} />
                         <h3>{anime.title}</h3>
                         <Link to = {`/character/${anime.mal_id}`}>{anime.title}</Link>
-                        <button onClick={() => addFavorite(anime)}>Add to favorites</button>
+                        <button onClick={() => addFavorite(anime)}>Agregar a Favoritos</button>
                     </li>
                 ))}
             </ul>
+            <div className='pagination'>
+                <button onClick={handlePreviousPage} disabled={currentPage === 1}>Anterior</button>
+                <span>Pagina {currentPage} de {totalPages}</span>
+                <button onClick={handleNextPage} disabled={currentPage === totalPages}>Siguiente</button>
+            </div>
         </div>
     );
 }
-
 export default AnimeList;

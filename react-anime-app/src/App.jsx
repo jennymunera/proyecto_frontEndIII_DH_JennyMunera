0f9afd4d866be740a9efc30d65f2d00797563d02
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-11-30 21:36:30 -0500

    Add Header and Footer and Add logo for page

--- react-anime-app/src/App.jsx
+++ react-anime-app/src/App.jsx
@@ -1,31 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import Product from './pages/Product';
 import AnimeDetails from './components/AnimeDetails';
 import Navbar from './components/Navbar';
 import Favorites from './pages/Favorites';
+import Information from './pages/Information';
 import './styles/App.css';
+import image from './assets/image.png';
 
 function App() {
   return (
     <Router>
       <header>
-        <h1>Welcome to Anime World</h1>
-        <p>Find your favorite anime and add them to your collection!</p>
+        <div className="header-content">
+          <img src={image} alt="otakuhaven logo" className='logo' />
+          <div>
+            <h1>OtakuHaven</h1>
+            <p>Encuentra tus Animes Favoritos, y Agregalos a tú Colección</p>
+          </div>
+        </div>
       </header>
       <Navbar />
       <main className="container">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/product" element={<Product />} />
+          <Route path="/Information" element={<Information />} />
           <Route path="/character/:id" element={<AnimeDetails />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
       <footer>
-        <p>&copy; 2024 Anime World. All rights reserved.</p>
+        <p>&copy; 2024 OtakuHaven All rights reserved.</p>
       </footer>
     </Router>
   );

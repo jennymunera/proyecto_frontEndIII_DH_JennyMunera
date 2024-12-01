a579912aa791997357e2bc8c2c164c7d190ad9d0
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-11-30 21:39:36 -0500

    Add page information

---
+++ react-anime-app/src/pages/Information.jsx
@@ -0,0 +1,16 @@
+
+function Information() {
+    return (
+        <div className="information">
+            <h2>Acerca de OtakuHaven</h2>
+            <p>
+            ¡Bienvenidos a OtakuHaven! Nos apasiona traerles los mejores y más recientes animes, desde los clásicos favoritos hasta los nuevos lanzamientos que son tendencia. Nuestra misión es brindarles a los entusiastas del anime una plataforma atractiva para explorar y seguir sus series favoritas.
+            </p>
+            <p>
+            Ya seas fanático desde hace mucho tiempo o recién llegado al mundo del anime, estamos aquí para hacer que tu experiencia sea agradable e informativa. ¡Gracias por ser parte de nuestra comunidad!
+            </p>
+        </div>
+    );
+}
+
+export default Information;
--- react-anime-app/src/pages/Product.jsx
+++
@@ -1,5 +0,0 @@
-function Product() {
-    return <h1>Our Products</h1>;
-}
-
-export default Product;

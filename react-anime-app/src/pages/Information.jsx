b09da4353514fefabbd662af23456865c1978acf
Jenny Munera Serna <112194662+jennymunera@users.noreply.github.com>
2024-12-04 00:03:41 -0500

    organize information and add logo

--- react-anime-app/src/pages/Information.jsx
+++ react-anime-app/src/pages/Information.jsx
@@ -1,13 +1,21 @@
+import image from '../assets/image.png';
 
 function Information() {
     return (
         <div className="information">
+            <img src={image} alt="otakuhaven logo" className='logo' />
             <h2>Acerca de OtakuHaven</h2>
+            <h3>
+            ¡Bienvenidos a OtakuHaven! 
+            </h3>
             <p>
-            ¡Bienvenidos a OtakuHaven! Nos apasiona traerles los mejores y más recientes animes, desde los clásicos favoritos hasta los nuevos lanzamientos que son tendencia. Nuestra misión es brindarles a los entusiastas del anime una plataforma atractiva para explorar y seguir sus series favoritas.
+            Nos apasiona traerles los mejores y más recientes animes, desde los clásicos favoritos hasta los nuevos lanzamientos que son tendencia. Nuestra misión es brindarles a los entusiastas del anime una plataforma atractiva para explorar y seguir sus series favoritas.
             </p>
             <p>
-            Ya seas fanático desde hace mucho tiempo o recién llegado al mundo del anime, estamos aquí para hacer que tu experiencia sea agradable e informativa. ¡Gracias por ser parte de nuestra comunidad!
+            Ya seas fanático desde hace mucho tiempo o recién llegado al mundo del anime, estamos aquí para hacer que tu experiencia sea agradable e informativa.
+            </p>
+            <p>
+            ¡Gracias por visitar OtakuHaven!
             </p>
         </div>
     );
